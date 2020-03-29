b66c899e0149beb76f36734c5b249043c190e7e1
XST <dadelaricaar@gmail.com>
2020-03-30 01:58:52 +0200

    Init

---
+++ src/main/java/com/github/dimarts/tournabross/TournabrossAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.dimarts.tournabross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournabrossAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournabrossAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/dimarts/tournabross/TournabrossAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dimarts.tournabross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournabrossAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
